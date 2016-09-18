c6a7b50ce766d046af4db87bb8f9ea77709d3c73
Ismail Kocdemir <ihkocdemir@gmail.com>
2016-09-18 11:20:10 +0300

    first commmit

---
+++ src/main/java/org/ismail/et/impl/MidiNote.java
@@ -0,0 +1,20 @@
+package org.ismail.et.impl;
+
+import javax.sound.midi.*;
+import java.util.logging.*;
+
+public class MidiNote {
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void playNote(){
+
+		MidiEvent midEvent = new MidiEvent(null, 0);
+		
+	}
+	
+}
